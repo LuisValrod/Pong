ef69efc0cfdb98828430fadb08e0c962db5b46ec
LuisValrod <luisrodriguezvalido@gmail.com>
2024-02-01 16:40:44 +0000

    Paddles created

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 screen = Screen()
 
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
 screen.title('Pong-Game')
+screen.tracer(0)
+
+screen.listen()
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-paddle = Turtle('square')
-paddle.color('white')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
+screen.onkeypress(r_paddle.go_up,'Up')
+screen.onkeypress(r_paddle.go_down,'Down')
+screen.onkeypress(l_paddle.go_up,'w')
+screen.onkeypress(l_paddle.go_down,'s')
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
 
 
 screen.exitonclick()
