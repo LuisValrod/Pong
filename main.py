b576348fb5c1a45c8246a1b77ea30dc209cc80b0
LuisValrod <luisrodriguezvalido@gmail.com>
2024-02-01 11:02:47 +0000

    class created

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ screen = Screen()
 
 screen.bgcolor('black')
 screen.setup(width=800, height=600)
+screen.title('Pong-Game')
+
+
+paddle = Turtle('square')
+paddle.color('white')
+paddle.shapesize(stretch_wid=5, stretch_len=1)
 
 
 screen.exitonclick()
