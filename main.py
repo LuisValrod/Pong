914edfb8759fb136699e739411acf45b5292e3d1
LuisValrod <luisrodriguezvalido@gmail.com>
2024-02-03 10:23:54 +0000

    README

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 
@@ -15,6 +17,7 @@ screen.listen()
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.onkeypress(r_paddle.go_up,'Up')
@@ -26,7 +29,7 @@ screen.onkeypress(l_paddle.go_down,'s')
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
     #Detect collision with wall
@@ -38,11 +41,16 @@ while game_is_on:
     if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_x()
 
+
+    #Detect R paddle misses:
+
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
 
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.r_point()
 
 
 
